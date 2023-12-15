f13250c5b430a6035e49191ffc0996e80674600b
VuNgocTuyenK2 <tuyeni02km2@gmail.com>
2023-12-15 19:07:55 +0700

    Upload Excel

--- MVC/Controllers/ClothesController.cs
+++ MVC/Controllers/ClothesController.cs
@@ -8,12 +8,15 @@ using Microsoft.EntityFrameworkCore;
 using MVC.Data;
 using MVC.Models;
 using OfficeOpenXml;
+using MVC.Models.Process;
 using X.PagedList;
 namespace MVC.Controllers
 {
     public class ClothesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        
+        private ExcelProcess _excelProcess = new ExcelProcess();
 
         public ClothesController(ApplicationDbContext context)
         {
@@ -116,6 +119,53 @@ namespace MVC.Controllers
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+        //Vu Ngoc Tuyen 2021050715 lam phan uploadexcel
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file!=null)
+                {
+                    string fileExtension = Path.GetExtension(file.FileName);
+                    if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                    {
+                        ModelState.AddModelError("", "Please choose excel file to upload!");
+                    }
+                    else
+                    {
+                        //rename file when upload to server
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", "File" + DateTime.Now.Day + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Millisecond + fileExtension);
+                        var fileLocation = new FileInfo(filePath).ToString();
+                        if (file.Length > 0)
+                        {
+                            using (var stream = new FileStream(filePath, FileMode.Create))
+                            {
+                                //save file to server
+                                await file.CopyToAsync(stream);
+                                //read data from file and write to database
+                                var dt = _excelProcess.ExcelToDataTable(fileLocation);
+                                for(int i = 0; i < dt.Rows.Count; i++)
+                                {
+                                    var clt = new Clothes();
+                                    clt.ClothesID = dt.Rows[i][0].ToString();
+                                    clt.ClothesName = dt.Rows[i][1].ToString();
+                                    clt.Number = dt.Rows[i][2].ToString();
+                                    clt.Color = dt.Rows[i][3].ToString();
+                                       
+                                    _context.Add(clt);
+                                }
+                                await _context.SaveChangesAsync();
+                                return RedirectToAction(nameof(Index));
+                            }
+                        }
+                    }
+                }
+
+            return View();
         }
 
         /// <summary>
